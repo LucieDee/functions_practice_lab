5a79302f92307fe0b0b00eadbb4d4337445bdcfd
Lucie <lucie@Lucies-MacBook-Pro.local>
2017-02-14 18:15:49 +0000

    initial commit

---
+++ ruby_functions_practice.rb
@@ -0,0 +1,41 @@
+def add(num_1, num_2)
+  return (num_1 + num_2)
+end
+
+# puts add(1, 2)
+
+def subtract(num_1, num_2)
+  return (num_1 - num_2)
+end
+
+# puts subtract(10, 5)
+
+def multiply(num_1, num_2 )
+  return (num_1 * num_2)
+end
+
+# puts multiply(4, 2)
+
+def divide(num_1, num_2)
+  return (num_1 / num_2)
+end
+
+# puts divide(10, 2)
+
+def length_of_string(string)
+  return string.length
+end
+
+# puts length_of_string("A string of length 21")
+
+def join_string(string_1, string_2)
+  return string_1.chomp + string_2
+end
+
+# puts string_1 = ("Mary had a little lamb,"), string_2 = ("it's fleece was white as snow.")
+
+def add_string_as_number(num_1, num_2)
+  return (num_1).to_i + (num_2).to_i
+end
+
+# puts add_string_as_number("1", "2")
